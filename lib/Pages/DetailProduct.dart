4d132bb19c1c95e79570359e94149498b4291099
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-08-25 21:38:54 +0430

    adding WillPopScope widget to DetailProduct.dart class to handle pop back and send data back.

--- lib/Pages/DetailProduct.dart
+++ lib/Pages/DetailProduct.dart
@@ -9,32 +9,38 @@ class DetailProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(title),
-            ),
-            Container(
-              child: RaisedButton(
-                padding: EdgeInsets.all( 0.05),
-                child: Text('Delete'),
-                color: Colors.red,
-                onPressed: () => Navigator.pop(context, true),
+    return WillPopScope(
+      onWillPop:() {
+        print('back button pressed');
+        Navigator.pop(context,false);
+        return Future.value(false);
+      },
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
+              centerTitle: true,
             ),
-          ],
-        ));
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(imageUrl),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(title),
+                ),
+                Container(
+                  child: RaisedButton(
+                    padding: EdgeInsets.all(0.05),
+                    child: Text('Delete'),
+                    color: Colors.red,
+                    onPressed: () => Navigator.pop(context, true),
+                  ),
+                ),
+              ],
+            )));
   }
 }
