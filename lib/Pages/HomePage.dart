44f2ebd30a9388b8a0d3a66dd92a1fe153e009da
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-08-26 17:32:30 +0430

    drawer for homePage class

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -8,6 +8,20 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
    return Scaffold(
+     drawer: Drawer(
+       child : Column(
+         children: <Widget>[
+           AppBar(
+             title : Text('Choose'),
+             automaticallyImplyLeading: false,
+           ),
+           ListTile(
+             title: Text('Manage Products'),
+             onTap: () => {},
+           )
+         ],
+       ),
+     ),
      appBar: AppBar(
        title: Text(
          s,
