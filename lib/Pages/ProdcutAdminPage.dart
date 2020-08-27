0a97abee8342d4602c6bc8e75115361cb18f8f8f
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-08-27 21:56:22 +0430

    adding two tabs in productAdminPage class.

--- lib/Pages/ProdcutAdminPage.dart
+++ lib/Pages/ProdcutAdminPage.dart
@@ -1,42 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:helloflutter/Pages/Product_Create.dart';
+import 'package:helloflutter/Pages/Product_list.dart';
 
 import 'HomePage.dart';
 
 class ProductAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(
-              title: Text('Choose'),
-              automaticallyImplyLeading: false,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  title: Text('Choose'),
+                  automaticallyImplyLeading: false,
+                ),
+                ListTile(
+                  title: Text('All Products'),
+                  onTap: () => {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => HomePage()),
+                    )
+                  },
+                ),
+              ],
             ),
-            ListTile(
-              title: Text('All Products'),
-              onTap: () => {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) => HomePage()),
+          ),
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.create),
+                  text: 'Create Products',
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                  text: 'My Products',
                 )
-              },
-            )
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        title: Text(
-          'Manage Products',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Manage Your Products'),
-      ),
+              ],
+            ),
+            title: Text(
+              'Manage Products',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+          ),
+          body: TabBarView(
+            children: <Widget>[ProductCreatePage(), ProductListPage()],
+          )),
     );
   }
 }
